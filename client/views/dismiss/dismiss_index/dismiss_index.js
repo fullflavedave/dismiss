aaab43a334778bc5463d7fa0712cec033a1b0741
Dave Allen <david.allen@meliortechnology.com>
2014-06-30 10:49:45 -0700

    Refactored to isolate data for page, questions, and next pages

--- client/views/dismiss/dismiss_index/dismiss_index.js
+++ client/views/dismiss/dismiss_index/dismiss_index.js
@@ -2,79 +2,184 @@
 /* DismissIndex: Event Handlers and Helpers */
 /*****************************************************************************/
 
-var htmlSections =
-[
-    {
-        id: 'section1',
-        html:
-            ' <h2>I. Baseline eligibility: Were you convicted of a criminal offense?</h2><form class="form-horizontal" role="form"><div class="form-group"><label class="col-sm-2 control-label"> 1.Did you receive a prison sentence for your conviction </label><div class="col-sm-10"><div class="radio-inline"><label><input type="radio" name="q1-radios" id="q1-yes" value="true" checked> Yes </label></div><div class="radio-inline"><label><input type="radio" name="q1-radios" id="q1-no" value="false"> No </label></div></div></div><div class="form-group"><label class="col-sm-2 control-label"> 2.Were you convicted of a serious sex offense (Cal. Penal Code sections 586, 288, 288a, 288.5, 289, or 261.5) OR failing to obey a police officer (Vehicle Code section 42001) </label><div class="col-sm-10"><div class="radio-inline"><label><input type="radio" name="q2-radios" id="q2-yes" value="true" checked> Yes </label></div><div class="radio-inline"><label><input type="radio" name="q2-radios" id="q2-no" value="false"> No </label></div></div></div><div class="form-group"><label class="col-sm-2 control-label"> 3.Are you currently facing charges? </label><div class="col-sm-10"><div class="radio-inline"><label><input type="radio" name="q3-radios" id="q3-yes" value="true" checked> Yes </label></div><div class="radio-inline"><label><input type="radio" name="q3-radios" id="q3-no" value="false"> No </label></div></div></div><div class="form-group"><label class="col-sm-2 control-label"> 4.Are you currently on probation or parole? </label><div class="col-sm-10"><div class="radio-inline"><label><input type="radio" name="q4-radios" id="q4-yes" value="true" checked> Yes </label></div><div class="radio-inline"><label><input type="radio" name="q4-radios" id="q4-no" value="false"> No </label></div></div></div>'
-            + createFormButtons('section1', 'section2')
-            +'</form>'
-    },
-    {
-        id: 'rehabCert',
-        html:
-                'You are ineligible for a dismissal. However, you MAY BE ELIGIBLE for a Certificate of Rehabilitation.'
-                + '[See chart to check for eligibility for a Certificate of Rehabilitation]'
-                + createFormButtons(null, 'section1')
+var answers = [];
+var history = [];
+var pages = {
+    p1: {
+        title: 'I. Baseline Eligibility',
+        questions: {
+            q0: 'Did you receive a prison sentence for your conviction?',
+            q1: 'Were you convicted of a serious sex offense (Cal. Penal Code sections 586, 288, 288a, 288.5, 289, or 261.5) OR failing to obey a police officer (Vehicle Code section 42001)?',
+            q2: 'Are you currently facing charges?',
+            q3: 'Are you currently on probation or parole?',
 
+        },
+        yesPage: 'rehabCert',
+        noPage: 'p2'
     },
-    {
-        id: 'section2',
-        html:
-                '<h2>II. Misdemeanor or Infraction</h2><form class="form-horizontal" role="form"><div class="form-group">'
-                + '<label class="col-sm-2 control-label"> Were you convicted of either a misdemeanor or an infraction? </label><div class="col-sm-10"><div class="radio-inline">'
-                + '<label><input type="radio" name="q5-radios" id="q1-yes" value="true" checked> Yes </label></div><div class="radio-inline"><label><input type="radio" name="q5-radios" id="q1-no" value="false"> No </label></div></div></div>'
-                + createFormButtons('section2', 'section1')
-                + '</form>'
-
+    rehabCert: {
+        title: 'Ineligible',
+        text:  'You are ineligible for a dismissal. However, you MAY BE ELIGIBLE for a Certificate of Rehabilitation.' +
+               '[See chart to check for eligibility for a Certificate of Rehabilitation]'
     },
-    {
-        id: 'section3',
-        html:
-                '<h2>III. Section 3</h2>'
-                + '<form>'
-                + createFormButtons('section3', 'section2')
-                + '</form>'
+    p2: {
+        title: 'II. Misdemeanor or Infraction',
+        questions: {
+            q4: 'Were you convicted of either a misdemeanor or an infraction?'
+        },
+        yesPage: 'p3',
+        noPage: 'p4'
     },
-    {
-        id: 'section4',
-        html:
-                '<h2>IV. Section 4</h2>'
-                + '<form>'
-                + createFormButtons('section3', 'section2')
-                + '</form>'
+    p3: {
+        title: 'III. Probation',
+        questions: {
+            q5: 'If you were convicted of a misdemeanor, did you receive probation?'
+        },
+        yesPage: 'p4',
+        noPage: 'p3a'
     },
-    {
-        id: 'section5',
-        html:
-                '<h2>V. Section 5</h2>'
+    p3a: {
+        title: 'III.a. Probation Fines and Fees',
+        questions: {
+            q6: 'If you did not receive probation, did you pay off all your court fines and fees?'
+        },
+        yesPage: 'p3b',
+        noPage: 'p4'
+    },
+    p3b: {
+        title: 'III.b. One Year Past',
+        questions: {
+            q7: 'If you paid off all your fines and fees, has it been a year since the date of your conviction?'
+        },
+        yesPage: 'eligible12034a',
+        noPage: 'mayQualify12034'
+    },
+    eligible12034a: {
+        title: 'Eligible',
+        text: 'YOU ARE ELIGIBLE for a mandatory 1203.4a dismissal. Fill out form CR-180 to begin the filing process for your petition. [link to form CR-180.]'
+    },
+    mayQualify12034: {
+        title: 'Discretionary Dismissal',
+        text: 'YOU MAY QUALIFY for a discretionary 1203.4 dismissal. Fill out form CR-180 to begin the filing process for your petition. [link to form CR-180.] ' +
+              'or consider waiting out the one year period for a mandatory dismissal.'
+    },
+    p4: {
+        title: 'IV. Probation Violation',
+        questions: {
+            q8: 'If you were sentenced to probation for your misdemeanor OR felony conviction, did you violate the terms of your probation?'
+        },
+        yesPage: 'p4a',
+        noPage: 'seemsWrong'
+    },
+    p4a: {
+        title: '1203.4 Dismissal',
+        text: 'YOU MAY QUALIFY for a discretionary 1203.4 dismissal. Fill out form CR-180 to begin the filing process for your petition. [link to form CR-180.]' +
+            'If you were convicted of a felony, go on to <a href="#" id="linkTo5">question V</a> to see if you are eligible for a 17(b) felony reduction.'
+    },
+    seemsWrong: {
+        title: '1203.4 Dismissal',
+        text: 'YOU MAY QUALIFY for a discretionary 1203.4 dismissal. Fill out form CR-180 to begin the filing process for your petition. [link to form CR-180.]' +
+                'If you were convicted of a felony, go on to <a href="#" id="linkTo5">question V</a> to see if you are eligible for a 17(b) felony reduction.'
+    },
+    p5: {
+      title: 'V. Felony Wobbler',
+        questions: {
+            q9: 'Were you convicted of a felony wobbler? [A wobbler conviction is a conviction that is punishable as a felony OR a misdemeanor.]'
+        },
+        yesPage: 'wobblerYes',
+        noPage: 'wobblerNo'
+    },
+    wobblerYes: {
+        title: '17(b) Felony Reduction',
+        text: 'If you were convicted of a felony under a wobbler statute, you may be eligible for a 17(b) felony reduction. ' +
+              'You can ask for a 17(b) reduction in your 1203.4 dismissal petition. Fill out form CR-180 to begin the filing process for your petition. ' +
+              '[link to form CR-180.]'
+    },
+    wobblerNo: {
+        title: '1203.4 Dismissal',
+        text: 'You may still petition for a 1203.4 dismissal. Fill out form CR-180 to begin the filing process for your petition. [link to form CR-180.]'
     }
-];
+};
 
-function displayHTML(sectionID) {
-    console.log('Displaying sectionID = ' + sectionID);
-    for (var i=0; i < htmlSections.length; i++) {
-        if (htmlSections[i].id === sectionID) {
-            $('#wizard-text').html(htmlSections[i].html);
-            return;
-        }
+function displayHTML(pageId) {
+    console.log('Displaying pageId = ' + pageId);
+    if (history[history.length-1] !== pageId) { // for case of going backwards
+        history.push(pageId);
     }
+    var htmlText =
+            ' <h2>' + pages[pageId]['title'] + '</h2>' +
+            '<form class="form-horizontal" role="form">' +
+            createPageText(pages[pageId]['text']) +
+            createQAHTML(pages[pageId]['questions']) +
+            createFormButtonsHTML(pageId) +
+            '</form>';
+
+    $('#wizard-text').html(htmlText);
 }
 
-function createFormButtons(sectionID, backSectionID) {
+function createPageText(text) {
+    return text || '';
+}
+
+function createQAHTML(questions) {
+    var htmlString = '';
+    for (var qId in questions) {
+        htmlString += createQuestionHTML(questions[qId]);
+        htmlString += createAnswerInputHTML(qId);
+    }
+    return htmlString;
+}
+
+function createQuestionHTML(questionText) {
+    return '<div class="form-group"><label class="col-sm-2 control-label">' + questionText + '</label>';
+}
+
+function createAnswerInputHTML(questionId) {
+    var htmlString =
+            '<div class="col-sm-10"><div class="radio-inline">' +
+            '<label><input type="radio" name="' + questionId + '-radios" id="' + questionId + '-yes" value="true" checked> Yes </label>' +
+            '</div>' +
+            '<div class="radio-inline"><label><input type="radio" name="' + questionId + '-radios" id="' + questionId + '-no" value="false"> No </label>' +
+            '</div></div></div>';
+    return htmlString;
+}
+
+// Send in null to sectionID for no submit button in case of wizard end node
+function createFormButtonsHTML(sectionId) {
     var htmlString =
             '<div class="form-group" style="margin-top:20px;">'
             + '<div class="col-sm-offset-2 col-sm-10">';
-    if (backSectionID)
-        htmlString += '<button id="back" data-back-html-id="' + backSectionID + '" type="submit" class="btn btn-default" style="margin-right: 10px;">Back</button>'
+    htmlString += '<button id="back" type="submit" class="btn btn-default" style="margin-right: 10px;">Back</button>';
     htmlString += '<button id="start-over" type="submit" class="btn btn-default" style="margin-right: 10px;">Start Over</button>';
-    if (sectionID)
-        htmlString += '<button id="' + sectionID + '-submit" type="submit" class="btn btn-primary">Submit</button>';
+    if (sectionId) {
+        htmlString += '<button id="wizard-submit" type="submit" class="btn btn-primary">Submit</button>';
+    }
     htmlString +=
             '</div>'
             + '</div>';
+
     return htmlString;
+}
+
+function handleSubmit(e) {
+    e.preventDefault();
+    var result = '';
+    var currentPage = history[history.length-1];
+    var questions = pages[currentPage]['questions'];
+    var yesPage = pages[currentPage]['yesPage'];
+    var noPage = pages[currentPage]['noPage'];
+    for (var qId in questions) {
+        var answer = $('[name="' + qId + '-radios"]:checked').val();
+        answers.push({qId: answer});
+        if (truthy(answer)) {
+            result = true;
+        }
+    }
+    if(result) {
+        displayHTML(yesPage);
+    } else {
+        displayHTML(noPage);
+    }
 }
 
 Template.DismissIndex.events({
@@ -84,38 +189,23 @@ Template.DismissIndex.events({
    *
    *  }
    */
-    'click #section1-submit': function(e, tmpl) {
-        e.preventDefault();
-        var a1 = $('[name="q1-radios"]:checked').val();
-        var a2 = $('[name="q2-radios"]:checked').val();
-        var a3 = $('[name="q3-radios"]:checked').val();
-        var a4 = $('[name="q4-radios"]:checked').val();
-        if(truthy(a1) || truthy(a2) || truthy(a3) || truthy(a4)) {
-            console.log('rehabCert');
-            displayHTML('rehabCert');
-        } else {
-            console.log("On to q2");
-            displayHTML('section2');
-        }
+    'click #wizard-submit': function(e, tmpl) {
+        handleSubmit(e);
     },
 
-    'click #section2-submit': function(e, tmpl) {
+    'click #linkTo5': function(e, tmpl) {
         e.preventDefault();
-        var a5 = $('[name="q5-radios"]:checked').val();
-        if(truthy(a5)) {
-            console.log('section3');
-            displayHTML('section3');
-        } else {
-            console.log('section4');
-            displayHTML('section4');
-        }
+        displayHTML('p5');
     },
 
     'click #back': function(e, tmp) {
         e.preventDefault();
-        var backSectionID = $(e.currentTarget).data('back-html-id');
-//        console.log('backHTMLID = ' + backSectionID);
-        displayHTML(backSectionID);
+        history.pop();
+        displayHTML(history[history.length - 1]);
+    },
+
+    'click #start-over': function(e, tmp) {
+        location.reload();
     }
 });
 
@@ -136,7 +226,8 @@ Template.DismissIndex.created = function () {
 };
 
 Template.DismissIndex.rendered = function () {
-    displayHTML('section1');
+    displayHTML('p1');
+//    displayHTML('section1');
 };
 
 Template.DismissIndex.destroyed = function () {
